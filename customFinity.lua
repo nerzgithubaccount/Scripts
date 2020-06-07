5a030e272192d3acb4104321b916ccdf126a09a8
nerzgithubaccount <66380715+nerzgithubaccount@users.noreply.github.com>
2020-06-06 20:25:45 -0500

    Update customFinity.lua

--- customFinity.lua
+++ customFinity.lua
@@ -71,8 +71,8 @@ finity.dark_theme = { -- dark
 	checkbox_outer = Color3.fromRGB(84, 81, 86),
 	checkbox_inner = Color3.fromRGB(46, 46, 46),
 
-	slider_color = Color3.fromRGB(177, 177, 177),
-	slider_color_sliding = Color3.fromRGB(102, 176, 146),
+	slider_color = Color3.fromRGB(67, 148, 103),
+	slider_color_sliding = Color3.fromRGB(119, 224, 163),
 	slider_background = Color3.fromRGB(88, 84, 90),
 	slider_text = Color3.fromRGB(177, 177, 177),
 
